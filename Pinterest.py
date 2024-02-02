3751e3b828d5d446593c8959cadef777ffad40a8
nlx001 <111305514+nlx001@users.noreply.github.com>
2024-02-02 16:15:22 +0100

    Update Pinterest.py

--- Pinterest.py
+++ Pinterest.py
@@ -11,7 +11,6 @@ from RequestBuilder import RequestBuilder
 from requests.structures import CaseInsensitiveDict
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
