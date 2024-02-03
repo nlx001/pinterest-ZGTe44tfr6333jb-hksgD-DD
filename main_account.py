f2706d0c6781379fff692aef8b430927ca988881
nlx001 <111305514+nlx001@users.noreply.github.com>
2024-02-03 15:11:12 +0100

    Add files via upload

---
+++ main_account.py
@@ -0,0 +1,19 @@
+from Pinterest import Pinterest
+#from py3pin.Pinterest import Pinterest
+import pickle
+import random
+
+pinterest = Pinterest(email='theredditwonda@gmail.com',
+                      password='Baum96?!',
+                      username='coaxonstudios',
+                      cred_root='cred_root') #proxies={"http": "45.85.204.3:59101"}
+
+with open("all_files.pkl", "rb") as f:
+    fil = pickle.load(f)
+current_pin = random.choice(fil)
+
+pinterest.pin(image_url=f"https://pincontainer.blob.core.windows.net/30nightjobs/{current_pin}.png", board_id="928304610631143129", link="basicdime.com")
+
+fil.remove(current_pin)
+with open("all_files.pkl", "wb") as f:
+    pickle.dump(fil, f)
