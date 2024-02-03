11647edd4596ebb6f61713aa9f423ffc9fc35b18
nlx001 <111305514+nlx001@users.noreply.github.com>
2024-02-03 16:42:03 +0100

    Update main_account.py

--- main_account.py
+++ main_account.py
@@ -6,14 +6,14 @@ import random
 pinterest = Pinterest(email='theredditwonda@gmail.com',
                       password='Baum96?!',
                       username='coaxonstudios',
-                      cred_root='cred_root') #proxies={"http": "45.85.204.3:59101"}
+                      cred_root='/home/nilslux/pinterest/pinterest-ZGTe44tfr6333jb-hksgD-DD/cred_root') #proxies={"http": "45.85.204.3:59101"}
 
-with open("all_files.pkl", "rb") as f:
+with open("/home/nilslux/pinterest/pinterest-ZGTe44tfr6333jb-hksgD-DD/all_files.pkl", "rb") as f:
     fil = pickle.load(f)
 current_pin = random.choice(fil)
 
 pinterest.pin(image_url=f"https://pincontainer.blob.core.windows.net/30nightjobs/{current_pin}.png", board_id="928304610631143129", link="basicdime.com")
 
 fil.remove(current_pin)
-with open("all_files.pkl", "wb") as f:
+with open("/home/nilslux/pinterest/pinterest-ZGTe44tfr6333jb-hksgD-DD/all_files.pkl", "wb") as f:
     pickle.dump(fil, f)
