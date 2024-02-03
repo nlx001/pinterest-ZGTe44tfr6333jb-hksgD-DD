4b1139d1e091dfc494822b26e52fa9a19549be50
nlx001 <111305514+nlx001@users.noreply.github.com>
2024-02-03 16:46:15 +0100

    Update account_logger.py

--- account_logger.py
+++ account_logger.py
@@ -2,6 +2,6 @@ from Pinterest import Pinterest
 pinterest = Pinterest(email='theredditwonda@gmail.com',
                       password='Baum96?!',
                       username='coaxonstudios',
-                      cred_root='cred_root',
+                      cred_root='/home/nilslux/pinterest/pinterest-ZGTe44tfr6333jb-hksgD-DD/cred_root',
                       proxies={"http": "45.85.204.3:59101"})
 pinterest.login() #proxy="45.85.204.3:59101"
