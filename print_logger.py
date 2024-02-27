c144e0579c16b283193f3339ead5060a455c01d0
nlx001 <111305514+nlx001@users.noreply.github.com>
2024-02-27 21:59:44 +0100

    Add files via upload

---
+++ print_logger.py
@@ -0,0 +1,7 @@
+from Pinterest import Pinterest
+pinterest = Pinterest(email='theredditwonda@gmail.com',
+                      password='Baum96?!',
+                      username='coaxonstudios',
+                      cred_root='cred_root',
+                      proxies={"http": "168.158.231.170:59101"})
+pinterest.login() #proxy="45.85.204.3:59101"
